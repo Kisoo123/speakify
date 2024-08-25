2befec29026349f56d0192b5b30084ee652bd276
rltnt <rltntjqj@gmail.com>
2024-08-26 03:10:12 +0900

    ks 테스트용

--- src/main/java/com/project/demo/controller/HomeController.java
+++ src/main/java/com/project/demo/controller/HomeController.java
@@ -2,7 +2,6 @@ package com.project.demo.controller;
 
 import com.project.demo.model.service.DemoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +12,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HomeController {
     private final DemoService service;
     @RequestMapping("/demo")
-    public ResponseEntity<Integer> test(@RequestParam int memberId){
+    public String test(@RequestParam int memberId){
         System.out.println(memberId);
         int result = service.insertName(memberId);
-        return ResponseEntity.ok(result);
+        return "demo";
     }
     @GetMapping("/")
     public String home() {
