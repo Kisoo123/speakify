15fc17ae231ef367e8bc3e32a8068c3f6a8f0d8a
rltnt <rltntjqj@gmail.com>
2024-09-14 13:34:58 +0900

    ks 음성채팅 기능 개발중

--- src/main/java/com/project/demo/webSocket/controller/WebSocketConfig.java
+++ src/main/java/com/project/demo/webSocket/controller/WebSocketConfig.java
@@ -1,14 +1,17 @@
-package com.project.webSocket.controller;
+package com.project.demo.webSocket.controller;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
+@EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new SignalingSocketHandler(), "/signal").setAllowedOrigins("*");
+        registry.addHandler(new SignalingSocketHandler(), "/signal")
+                .setAllowedOrigins("*");
     }
 }
