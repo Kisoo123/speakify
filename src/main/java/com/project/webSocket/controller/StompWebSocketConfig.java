9657bc81833a5252b5531be6dd78d6104c463ad8
rltnt <rltntjqj@gmail.com>
2024-10-03 21:28:51 +0900

    http > https

--- src/main/java/com/project/webSocket/controller/StompWebSocketConfig.java
+++ src/main/java/com/project/webSocket/controller/StompWebSocketConfig.java
@@ -18,6 +18,6 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS(); // WebSocket 엔드포인트
+        registry.addEndpoint("/wss").withSockJS(); // WebSocket 엔드포인트
     }
 }
