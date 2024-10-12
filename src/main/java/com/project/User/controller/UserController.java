c9747de2838fc528213b5c7c468a0c88e933f8d9
rltnt <rltntjqj@gmail.com>
2024-10-13 00:29:21 +0900

    프로필 사진 변경기능 추가

--- src/main/java/com/project/User/controller/UserController.java
+++ src/main/java/com/project/User/controller/UserController.java
@@ -1,12 +1,20 @@
 package com.project.User.controller;
 
 import com.project.User.service.UserService;
+import com.project.aws.s3.service.S3Service;
 import com.project.security.model.dto.User;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
@@ -14,18 +22,25 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.project.alarm.service.AlarmService.getCurrentUser;
+
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class UserController {
+    @Value("${profile.picture.upload.dir}")
+    private String uploadDir;
 
+    private final ServletContext servletContext;
     private final UserService service;
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+    private final S3Service s3Service;
+
     @PostMapping("/search")
     @ResponseBody
     public List<User> searchUsersByKeyword(@RequestParam("keyword") String keyword) {
 
-        System.out.println(keyword +"controller Keyword");
         keyword = URLDecoder.decode(keyword, StandardCharsets.UTF_8);
-        System.out.println(keyword + " decoded 키워드");
         String displayName = keyword;
         String uniqueTag = null;
 
@@ -36,14 +51,35 @@ public class UserController {
                 uniqueTag = parts[1];
             }
         }
-        Map<String,Object> params = new HashMap<>();
+        Map<String, Object> params = new HashMap<>();
         params.put("displayName", displayName);
         params.put("uniqueTag", uniqueTag);
         return service.searchUsersByKeyword(params);
     }
+
     @PostMapping("/getFriendList")
     @ResponseBody
-    public List<User> getFriendList(@RequestParam int userId){
+    public List<User> getFriendList(@RequestParam int userId) {
         return service.getFriendList(userId);
+    }
+
+
+    @PostMapping("/updateProfile")
+    public String updateProfile(
+            @RequestParam(value = "profilePicture", required = false) MultipartFile profilePicture, // 파일 선택적
+            @RequestParam(value = "displayName", required = false) String displayName,                // 선택적 문자열
+            @RequestParam(value = "statusMessage", required = false) String statusMessage,
+            HttpServletRequest request) {
+        User user = getCurrentUser();
+
+        if (profilePicture != null) {
+            user.setProfilePictureUrl(s3Service.uploadFile(profilePicture));
+        }
+        user.setDisplayName(displayName);
+        user.setStatusMessage(statusMessage);
+
+        // 사용자 정보 업데이트
+        service.updateUserProfile(user);
+        return "redirect:/";
     }
 }
